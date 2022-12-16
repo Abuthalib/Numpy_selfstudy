2cf2d38e54e8a586a5b5bd27d98b4940644d10a7
Abuthalib <thaluthalib2000@gmail.com>
2022-12-16 17:50:03 +0530

    16/12/2022

---
+++ Sorting Array.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+
+print(np.sort(arr))
+
+
+#other data type
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+
+
+#boolean
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
